bc41b764c45673b491aa9469223c3c1a44dc69c5
Aziz <aziznal.dev@gmail.com>
2023-10-05 22:41:43 +0300

    create video player component with custom play controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,165 @@
-import Image from 'next/image'
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Pause, Play } from "lucide-react";
+import {
+  HTMLAttributes,
+  PropsWithChildren,
+  forwardRef,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-full flex flex-col items-center justify-center">
+      <CustomVideoPlayer>
+        <source src="/video.mp4" type="video/mp4" />
+      </CustomVideoPlayer>
+    </div>
+  );
+}
+
+const CustomVideoPlayer = ({ children }: PropsWithChildren) => {
+  const ref = useRef<HTMLVideoElement>(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const formattedCurrentTime = useMemo(() => {
+    const hours = Math.floor(currentTime / 3600);
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = currentTime - minutes * 60;
+
+    let formattedSeconds = `${seconds}`;
+
+    if (seconds < 10) {
+      formattedSeconds = `0${seconds}`;
+    }
+
+    return hours
+      ? `${hours}:${minutes}:${formattedSeconds}`
+      : `${minutes}:${formattedSeconds}`;
+  }, [currentTime]);
+
+  const formattedVideoDuration = useMemo(() => {
+    if (!ref.current) return "00:00";
+
+    const video = ref.current;
+
+    const minutes = Math.floor(video.duration / 60);
+    const seconds = Math.round(video.duration - minutes * 60);
+
+    return `${minutes}:${seconds}`;
+  }, [ref]);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const video = ref.current;
+
+    const playEventListener = () => {
+      setIsPlaying(true);
+    };
+
+    const pauseEventListener = () => {
+      setIsPlaying(false);
+    };
+
+    const timeUpdateEventListener = () => {
+      setCurrentTime(Math.round(video.currentTime));
+    };
+
+    video.addEventListener("play", playEventListener);
+    video.addEventListener("pause", pauseEventListener);
+    video.addEventListener("timeupdate", timeUpdateEventListener);
+
+    return () => {
+      video.removeEventListener("play", playEventListener);
+      video.removeEventListener("pause", pauseEventListener);
+      video.addEventListener("timeupdate", timeUpdateEventListener);
+    };
+  }, []);
+
+  const play = () => {
+    console.info("playing");
+    ref.current?.play();
+  };
+
+  const pause = () => {
+    console.info("pausing");
+    ref.current?.pause();
+  };
+
+  const togglePlayPause = () => {
+    console.info("toggling play/pause");
+    isPlaying ? pause() : play();
+  };
+
+  const toggleFullscreen = () => {
+    ref.current?.requestFullscreen({
+      navigationUI: "hide",
+    });
+  };
+
+  return (
+    <div
+      className="relative text-white"
+      onKeyDown={(e) => {
+        if (e.key === " ") {
+          togglePlayPause();
+        }
+      }}
+      tabIndex={0}
+    >
+      <video
+        className="w-[800px] h-[500px] object-cover"
+        onClick={togglePlayPause}
+        onDoubleClick={toggleFullscreen}
+        loop
+        ref={ref}
+      >
+        {children}
+      </video>
+
+      <div className="bottom-0 absolute w-full px-4 flex flex-col gap-2">
+        <progress
+          className="w-full h-1 bg-slate-800 rounded-full mt-2"
+          value={0}
+          max={100}
+        ></progress>
+
+        <div className="flex items-center gap-2 mb-2">
+          {/* Play/Pause Button */}
+          <ControlButton onClick={togglePlayPause}>
+            {isPlaying ? <Pause /> : <Play />}
+          </ControlButton>
+
+          <div>
+            {formattedCurrentTime} / {formattedVideoDuration}
+          </div>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+type ControlButtonProps = HTMLAttributes<HTMLButtonElement> & {};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const ControlButton = forwardRef<HTMLButtonElement, ControlButtonProps>(
+  (props, ref) => {
+    return (
+      <button
+        className={cn(
+          "bg-none rounded-full flex items-center justify-center text-white transition hover:bg-slate-800 p-2"
+        )}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+ControlButton.displayName = "ControlButton";
